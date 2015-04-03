d599cc1fabac1b549ff83c038b7de9be59f36d06
Tao Wei <taowei@buffalo.edu>
2015-04-03 17:19:44 -0400

    gstreamer

--- rover2015/scripts/gstreamer.py
+++ rover2015/scripts/gstreamer.py
@@ -6,14 +6,20 @@ import os
 
 from conf import HOME_IP
 
-def talker():
-    cmd = '''gst-launch-0.10 -evt souphttpsrc location='http://127.0.0.1:8080/stream?topic=/chatter' is_live=true timeout=5 ! multipartdemux ! image/jpeg, width=640, height=480, framerate=15/1 ! ffdec_mjpeg ! x264enc pass=qual quantizer=20 tune=zerolatency ! rtph264pay ! udpsink host=128.205.54.9 port=1234'''
+def gstreamer():
     cmd = '''gst-launch-0.10 -evt souphttpsrc location='http://127.0.0.1:8080/stream?topic=/chatter' is_live=true timeout=5 ! multipartdemux ! image/jpeg ! ffdec_mjpeg ! x264enc pass=qual quantizer=20 tune=zerolatency ! rtph264pay ! udpsink host=%s port=1234''' % HOME_IP
     os.system(cmd)
-    rospy.init_node('gstreamer', anonymous=True)
-    r = rospy.Rate(10) # 10hz
-    while not rospy.is_shutdown():
-        r.sleep()
+    print 'gstreamer'
+
+def talker():
+
+    gstreamer()
+    
+#    rospy.init_node('gstreamer', anonymous=True)
+#    r = rospy.Rate(1) # 10hz
+#    while not rospy.is_shutdown():
+#        print 'gstreamer1'
+#        r.sleep()
         
 if __name__ == '__main__':
     try:
